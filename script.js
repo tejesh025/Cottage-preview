c8644e9bb0cde7d1149be0556ba4fff76768f634
tejesh025 <164521202+tejesh025@users.noreply.github.com>
2024-05-08 13:54:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,4 +23,32 @@ function shiftleft(){
     image.src = imagePaths[index]
     console.log(index);
 }
+setInterval(shiftright,3000);
+
+let customerindex = 0;
+let customerimagePaths = ["custom1.jpg","custom2.jpg","custom3.jpg","custom4.jpg","custom5.jpg","custom6.jpg","custom7.jpg","custom8.jpg","custom9.jpg" ];
+function shiftcright(){  
+    let image = document.querySelector('#customerimgimg');
+    customerindex++;  
+   
+    if(customerindex >= customerimagePaths.length)
+       {
+        customerindex= 0
+       } 
+    image.src = customerimagePaths[customerindex]
+    console.log(customerindex); 
+
+}
+function shiftcleft(){  
+    let image = document.querySelector('#customerimgimg');
+    customerindex--; 
+    if(customerindex < 0)
+       {
+        customerindex=customerimagePaths.length-1; 
+       }
+    image.src = customerimagePaths[customerindex]
+    console.log(customerindex);
+
+}
+setInterval(shiftcright,3000);
 
